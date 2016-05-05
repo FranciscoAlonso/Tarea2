8177b4c761dbaf4e6393e54cc0f0dc15d02123ca
francisco_alonso <francisco.a.alonso.s@gmail.com>
2016-05-05 19:56:00 -0400

    Commit

--- tarea2.R
+++ tarea2.R
@@ -88,7 +88,38 @@ top.n.custs <- function (data,cols,n=5) { #Requires some data frame and the top 
 
 
 
+duration
+campaign
+pdays
+previous
+emp.var.rate
+cons.price.idx
+cons.conf.idx
+euribor3m
+nr.employed
 
+
+bankDiscrete$age <- discretize(bank$age, method = "cluster", categories = 10)
+bankDiscrete$duration <- discretize(bank$duration, method = "cluster", categories = 10)
+bankDiscrete$campaign <- discretize(bank$campaign, method = "cluster", categories = 10)
+bankDiscrete$pdays <- discretize(bank$pdays, method = "cluster", categories = 10)
+bankDiscrete$previous <- discretize(bank$previous, method = "cluster", categories = 5)
+bankDiscrete$emp.var.rate <- discretize(bank$emp.var.rate, method = "cluster", categories = 10)
+bankDiscrete$cons.price.idx <- discretize(bank$cons.price.idx, method = "cluster", categories = 10)
+bankDiscrete$cons.conf.idx <- discretize(bank$cons.conf.idx, method = "cluster", categories = 10)
+bankDiscrete$euribor3m <- discretize(bank$euribor3m, method = "cluster", categories = 10)
+bankDiscrete$nr.employed <- discretize(bank$nr.employed, method = "cluster", categories = 10)
+model1 <- apriori(data = bankDiscrete, parameter = list(supp = 0.7, conf = 0.8))
+model2 <- apriori(data = bankDiscrete, parameter = list(supp = 0.7, conf = 0.8), appearance = list(rhs=c("y=No", "y=Yes")))
+
+rules.sorted <- sort(model3, by="lift")
+subset.matrix <- is.subset(rules.sorted, rules.sorted)
+subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
+redundant <- colSums(subset.matrix, na.rm=T) >= 1
+which(redundant)
+# remove redundant rules
+rules.pruned <- rules.sorted[!redundant]
+inspect(rules.pruned)
 
 
 
